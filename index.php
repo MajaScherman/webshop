b3482978e91a9c299feae72d6fafc305eb602c23
Amy <amy1001@niklasson.se>
2015-09-15 11:26:11 +0200

    Created buttons for navigation to the different pages.

--- index.php
+++ index.php
@@ -5,3 +5,13 @@ echo "</br>Buy or stuff!! </br> We sell JAVA and COOKIES ";
 
 
 ?>
+<html>
+  <form>
+  <input type="button" value="Logga in" onclick="window.location.href='login.php'" />
+  <input type="button" value="Registrera dig" onclick="window.location.href='new-user.php'" />
+  <input type="button" value="Items" onclick="window.location.href='items.php'" />
+  <input type="button" value="Varukorg" onclick="window.location.href='basket.php'" />
+
+  </form>
+
+</html>
