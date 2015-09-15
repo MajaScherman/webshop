009cbabd1f10882f864927508eca9907231e54c9
Amy <amy1001@niklasson.se>
2015-09-15 11:38:23 +0200

    Added header and footer to the index.php and created a title for the index.php

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
-<?php
-
-echo "Hello World!";
-echo "</br>Buy or stuff!! </br> We sell JAVA and COOKIES ";
-
-
-?>
+<?php require "inc/header.php"; ?>
 <html>
+  <body>
+    <h1>Welcome to Java & Cookies!</h1>
   <form>
   <input type="button" value="Logga in" onclick="window.location.href='login.php'" />
   <input type="button" value="Registrera dig" onclick="window.location.href='new-user.php'" />
@@ -13,5 +9,6 @@ echo "</br>Buy or stuff!! </br> We sell JAVA and COOKIES ";
   <input type="button" value="Varukorg" onclick="window.location.href='basket.php'" />
 
   </form>
-
+</body>
 </html>
+<?php require "inc/footer.php"; ?>
