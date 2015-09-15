d5d73a287492312afed940a26b31f32e3bb42f0e
Maria <littra@gmail.com>
2015-09-15 13:28:56 +0200

    new-user update

--- new-user.php
+++ new-user.php
@@ -1,10 +1,15 @@
 <?php require("inc/header.php") ?> 
 <h3>This is where you create a new user.</h3>
 <p>
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt illum iusto 
-perspiciatis expedita repellat corrupti quidem sit nemo tempore aperiam
- perferendis praesentium, aut, a ullam quod dignissimos, distinctio aspernatur. 
- Nesciunt!
+<form action="" method="post">
+<label>UserName :</label>
+<input type="text" name="username"/><br />
+<label>Password :</label>
+<input type="password" name="password"/><br/>
+<label>HomeAddress :</label>
+<input type="text" name="homeaddress"/><br/>
+<input type="submit" value=" Submit "/><br />
+</form>
 </p>
 
 <?php require("inc/footer.php");
