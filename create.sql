437fa54425ff8c43ff6597ce31bec441fa0ffb93
Maja <maja.scherman@gmail.com>
2015-09-15 14:03:03 +0200

    fixed error

--- create.sql
+++ create.sql
@@ -8,9 +8,10 @@ CREATE TABLE IF NOT EXISTS users(
 );
 
 CREATE TABLE IF NOT EXISTS orders(
-	username varchar(100) FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE,
+	username varchar(100) NOT NULL,
 	ordernbr serial NOT NULL,
-	CONSTRAINT order_pk PRIMARY KEY (username, ordernbr)
+	CONSTRAINT order_pk PRIMARY KEY (username, ordernbr),
+	CONSTRAINT username_fk FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE,
 );
 
 CREATE TABLE IF NOT EXISTS items(
@@ -20,7 +21,9 @@ CREATE TABLE IF NOT EXISTS items(
 );
 
 CREATE TABLE IF NOT EXISTS ordereditems(
-	itemnbr serial FOREIGN KEY (itemnbr) REFERENCES items(index) ON DELETE CASCADE,
-	ordernbr serial FOREIGN KEY (ordernbr) REFERENCES orders(ordernbr) ON DELETE CASCADE,
-	nbr int NOT NULL
+	itemnbr serial NOT NULL,
+	ordernbr serial NOT NULL,
+	nbr int NOT NULL,
+	CONSTRAINT itemnbr_fk FOREIGN KEY (itemnbr) REFERENCES items(index) ON DELETE CASCADE,
+	CONSTRAINT ordernbr_fk FOREIGN KEY (ordernbr) REFERENCES orders(ordernbr) ON DELETE CASCADE,
 );
