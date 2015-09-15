b6095d4bfc9ccf31b47d5964e3607cb4a25a327c
Philip Larsson <elt11pla@student.lu.se>
2015-09-15 11:33:26 +0200

    Add require header and footer

--- items.php
+++ items.php
@@ -1,3 +1,5 @@
+<?php require("inc/header.php"); ?>
+
 <h1>This is the item page.</h1>
 <p>
 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt illum iusto 
@@ -5,3 +7,5 @@ perspiciatis expedita repellat corrupti quidem sit nemo tempore aperiam
  perferendis praesentium, aut, a ullam quod dignissimos, distinctio aspernatur. 
  Nesciunt!
 </p>
+
+<?php require("inc/footer.php");
