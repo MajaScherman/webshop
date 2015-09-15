1d89b1e46ced64646e9d29d3bb1a4d0a2ca78108
Philip Larsson <elt11pla@student.lu.se>
2015-09-15 13:36:33 +0200

    Added whitespace in index

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php require "inc/header.php"; ?>
-    <h1>Welcome to Java & Cookies!</h1>
-  <form>
-  <input type="button" value="Login" onclick="window.location.href='login.php'" />
-  <input type="button" value="Register" onclick="window.location.href='new-user.php'" />
-  <input type="button" value="Items" onclick="window.location.href='items.php'" />
-  <input type="button" value="Basket" onclick="window.location.href='basket.php'" />
-  </form>
+
+<h1>Welcome to Java & Cookies!</h1>
+<form>
+    <input type="button" value="Login" onclick="window.location.href='login.php'" />
+    <input type="button" value="Register" onclick="window.location.href='new-user.php'" />
+    <input type="button" value="Items" onclick="window.location.href='items.php'" />
+    <input type="button" value="Basket" onclick="window.location.href='basket.php'" />
+</form>
+
 
 <?php require "inc/footer.php"; ?>
