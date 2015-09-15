ffff26a4b625c11b4f42b858e0b73ccf13902a48
Amy <amy1001@niklasson.se>
2015-09-15 14:23:18 +0200

    Fixed feedback when trying to login or registering.

--- login.php
+++ login.php
@@ -1,11 +1,31 @@
+<?php
+if ( $_SERVER["REQUEST_METHOD"] == "POST") {
+    $password = $_POST["password"];
+    $username = $_POST["username"];
+    if ( empty($password) || empty($username)) {
+        $status = "Missing email or username.";
+    } else {
+        // log in user
+        $status = "Thanks for logging in with $username and $password.";
+    }
+}
+?>
 
+<?php require("inc/header.php") ?>
 
 <h1> Login to JAVA and COOKIES awesome wepshop!!!<h1>
 
 <form action="" method="post">
-<label>UserName :</label>
-<input type="text" name="username"/><br />
-<label>Password :</label>
-<input type="password" name="password"/><br/>
-<input type="submit" value=" Submit "/><br />
+  <label for="username">UserName :</label>
+  <input type="text" name="username"/><br />
+  <label for="password">Password :</label>
+  <input type="password" name="password"/><br/>
+  <input type="submit" value=" Submit "/><br />
+
+  <?php if ( isset($status) ) : ?>
+  <?= $status ;?>
+  <?php endif ?>
+
 </form>
+
+<?php require("inc/footer.php") ?>
--- new-user.php
+++ new-user.php
@@ -1,15 +1,36 @@
-<?php require("inc/header.php") ?> 
+<?php
+if ( $_SERVER["REQUEST_METHOD"] == "POST") {
+    $password = $_POST["password"];
+    $username = $_POST["username"];
+    $homeaddress = $_POST["homeaddress"];
+    if ( empty($password) || empty($username)) {
+        $status = "Missing email or username.";
+    } else {
+        // log in user
+        $status = "Thanks for registering with $username, $password and $homeaddress.";
+    }
+}
+?>
+
+<?php require("inc/header.php") ?>
+
 <h3>This is where you create a new user.</h3>
 <p>
-<form action="" method="post">
-<label>UserName :</label>
-<input type="text" name="username"/><br />
-<label>Password :</label>
-<input type="password" name="password"/><br/>
-<label>HomeAddress :</label>
-<input type="text" name="homeaddress"/><br/>
-<input type="submit" value=" Submit "/><br />
-</form>
+  <form action="" method="post">
+    <label for="username">Username :</label>
+    <input type="text" name="username"/><br />
+    <label for="password">Password :</label>
+    <input type="password" name="password"/><br/>
+    <label for="homeaddress">Home address :</label>
+    <input type="text" name="homeaddress"/><br/>
+    <input type="submit" value=" Register "/><br />
+
+    <?php if ( isset($status) ) : ?>
+    <?= $status ;?>
+    <?php endif ?>
+  </form>
 </p>
 
-<?php require("inc/footer.php");
+
+
+<?php require("inc/footer.php");?>
