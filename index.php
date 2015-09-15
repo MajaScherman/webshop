308095e15b757023fa826bab9a2f34da48a55177
Amy <amy1001@niklasson.se>
2015-09-15 12:59:09 +0200

    Changed button names to English in index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php require "inc/header.php"; ?>
     <h1>Welcome to Java & Cookies!</h1>
   <form>
-  <input type="button" value="Logga in" onclick="window.location.href='login.php'" />
-  <input type="button" value="Registrera dig" onclick="window.location.href='new-user.php'" />
+  <input type="button" value="Login" onclick="window.location.href='login.php'" />
+  <input type="button" value="Registrer" onclick="window.location.href='new-user.php'" />
   <input type="button" value="Items" onclick="window.location.href='items.php'" />
-  <input type="button" value="Varukorg" onclick="window.location.href='basket.php'" />
+  <input type="button" value="Basket" onclick="window.location.href='basket.php'" />
   </form>
 
 <?php require "inc/footer.php"; ?>
