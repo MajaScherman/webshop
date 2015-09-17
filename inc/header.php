94b05119c357ba1cf75ce5806b71aa3e6feee07f
Philip Larsson <elt11pla@student.lu.se>
2015-09-17 11:24:57 +0200

    Add menu and css styling for menu

--- inc/header.php
+++ inc/header.php
@@ -1,11 +1,22 @@
 <!doctype html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8"/>		
+		<meta charset="UTF-8"/>
 		<link href="css/style.css" rel="stylesheet" type="text/css"/>
 		<title>Java and Cookies!</title>
 	</head>
 <body>
-<div id="header">	
+<div id="header">
 <h2>Welcome to Java&&Cookies!</h2>
 </div>
+
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="new-user.php">Register</a></li>
+        <li><a href="items.php">Items</a></li>
+        <li><a href="basket.php">Basket</a></li>
+    </ul>
+</nav>
+</br>
