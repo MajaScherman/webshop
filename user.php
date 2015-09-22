1b6516b387c2699b58ce1419bf56b8964e59f8b1
Philip Larsson <elt11pla@student.lu.se>
2015-09-22 15:19:08 +0200

    Add user info on user page and some minor css styling

--- user.php
+++ user.php
@@ -10,6 +10,10 @@ $db = new Database($config);
 if ( isset($_SESSION["username"]) ) {
     $username = $_SESSION["username"];
     $user = $db->getUserByusername($username);
+    $email = $user["email"];
+    $givenname = $user["givenname"];
+    $surname = $user["surname"];
+    $homeaddress = $user["homeaddress"];
 } else {
     // No session. Redirect.
     header("Location: login.php");
@@ -24,10 +28,14 @@ if ( isset($_SESSION["username"]) ) {
 <h1>User Page</h1>
 <p>Welcome to your user page! Here you can find almost nothing atm...</p>
 
-<?php if ( isset ($username) ) : ?>
+<?php if ( isset ($user) ) : ?>
     <div class="user-info">
         <ul>
             <li><strong>Username: </strong> <?= $username;  ?> </li>
+            <li><strong>Email:</strong> <?= $email; ?></li>
+            <li><strong>Givenname:</strong> <?= $givenname; ?></li>
+            <li><strong>Surname: </strong> <?= $surname?></li>
+            <li><strong>Homeaddress: </strong> <?= $homeaddress; ?></li>
         </ul>
     </div>
 <?php endif; ?>
