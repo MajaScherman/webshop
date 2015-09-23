c23ee6782d8b66df39b2973e1f64e65e045ede19
Philip Larsson <elt11pla@student.lu.se>
2015-09-23 19:08:18 +0200

    Change some swenglish

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>Site created by Philip Larsson, Amy Niklasson, Maria Persson och Maja Scherman</p>
+    <p>Site created by Philip Larsson, Amy Niklasson, Maria Persson and Maja Scherman</p>
 </footer>
 </body>
 </html>
