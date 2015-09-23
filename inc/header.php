d38a9f1ca5ad3a356d21c965de9c972f7ae2c59a
Philip Larsson <elt11pla@student.lu.se>
2015-09-23 18:51:53 +0200

    Add alternative menu depending on if user is logged in or not
    Create two similar menues in header, one for if the user is logged in and another if the user isn't logged in.

--- inc/header.php
+++ inc/header.php
@@ -5,22 +5,32 @@
 		<link href="css/style.css" rel="stylesheet" type="text/css"/>
 		<title>Java and Cookies!</title>
 	</head>
-<body>
-<img src = "images/headerimage.jpg"; width=800px>
+    <body>
+        <img src = "images/headerimage.jpg"; width=800px>
 
-<nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="new-user.php">Register</a></li>
-        <li><a href="items.php">Items</a></li>
-        <li><a href="basket.php">Basket</a></li>
-    </ul>
-    <?php if( isset($_SESSION["username"] ) ) : ?>
-        <div class="username">
-            <?= "You are logged in as: " . $_SESSION["username"]; ?>
-        </div>
-    <?php endif; ?>
-</nav>
-
+        <nav>
+            <ul>
+                <!-- Två olika menyer beroende på om man är inloggad eller ej.  -->
+                <?php if ( isset($_SESSION["username"]) ) : ?>
+                    <!-- Inloggad  -->
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="items.php">Items</a></li>
+                    <li><a href="basket.php">Basket</a></li>
+                    <li><a href="user.php">User Page</a></li>
+                <?php else : ?>
+                    <!-- Ej Inloggad -->
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="new-user.php">Register</a></li>
+                    <li><a href="items.php">Items</a></li>
+                    <li><a href="basket.php">Basket</a></li>
+                <?php endif; ?>
+            </ul>
+            <!-- Visa användarnamnet under menyn -->
+            <?php if( isset($_SESSION["username"] ) ) : ?>
+                <div class="username">
+                    <?= "You are logged in as: " . $_SESSION["username"]; ?>
+                </div>
+            <?php endif; ?>
+        </nav>
 </br>
